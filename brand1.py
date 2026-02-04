2c272846adcb298373690a16df5959d2fc86ad55
jenniferlopez236274-coder <jenniferlopez236274@gmail.com>
2026-02-04 21:01:31 +0500

    Create brand1.py

---
+++ brand1.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# Protected by FAIZAN ULTRA PYTHON PROTECTOR v3.0
+# Telegram: @sametoyouuu
+# WhatsApp: +92 370 6058550
+
+import zlib, lzma, bz2, base64, sys
+
+# Anti-debug
+if sys.gettrace(): sys.exit()
+
+_frceqghvamsqywsf = [
+        "ymo6eUNtv2rUXiv6maL3RHA2rnL616U",
+        "gXjb0PPqT6Wr1e3IxLFjXNcg9CZG",
+        "ait5bTWj7FMfE5CtEOfl/xe3mZuUJGx5JD6sjjxQ0i/2wr0o",
+        "zPepxiLnhCS8S5GyS6/wPy1sIGyJWrDDrW0WwgtfiGVcb6gSRuO",
+        "kEM3GOA+rN+QD+CQ1nJXz6asowNpnuo6tnYRLzMMi08mt8HNZ7",
+        "58S0abQNcTc8ZuZszx9HrvvBo3FJBu825K/gJ2cS5Rj2x9hS3kJhDU8G8nziNQ+Yn4",
+        "TheVgV3m3Wy4QYLw/SNsud23e",
+        "3EAI3GmnYLco7Arb8irEWTDC6OfnI/gA0NORqk",
+        "Nf2ZMZQdTklbs1t9QAK+OCy/xVLAMU1yLH/Zm0m4",
+        "jbL0MUFzH0peAlHxyyCGB342cD4VFkX",
+        "rG3JyoMScwzZA8vTzEHXchApKR3TW9lAFqDNw9QLTy/2CN+ys2hUE",
+        "/J32evuft5YAgEQYvMhjQc7nMexqAlO",
+        "ddYR+Ky4dmkY0g7X6MJUsFYrNQtKv0nJNiWhw6ajVGAp3QnE1vE4M26KA9yU3heyVW",
+        "MsE2zvKPUrJJfQhN8BMWvrf",
+        "Mm3sAZbMPMr0ez1CacODWkvH/RTh15ChZrQ32ZYNvUAx",
+        "uDgEUkz5AeBlu0YrnAJOBMIpUj8VUaGnbD1TE",
+        "IksCb//IIHJySLZvbupWLJQgY/Ox+wUvUCRvPTkMF3ZS",
+        "GkEYKm4X9KLu9nkunyXuYQMz5LfdsskAYJhMfeyV",
+        "1BPdU+hreDJkY3kALfl60U638NDARvrGOFI1+axZujVUYevjzR7ZQOTxqCCeu",
+        "W5mu1fycnOkD3Q+JZOpXKuchuP2TnfMbfkTYhNJBlzu+lVeUedzdT",
+        "Xd1z6yde/9bCwkRnZrgM9d+YMi4ZzA+Q8tARr1oaLAFh4U/hQxbn8snIQCow3Wf4",
+        "vrUNLhzNLYrG7W6/UQItEBPnaf87E4DuufdYORfx",
+        "NJnK/CA9N+g+hZqxEtVQNRdQQuxi5hJGkICF2AB7Y8hr/NTiBF9vgzfv0dxYtU96GRZ/0",
+        "ELGBAOGnZC4UkIXyG33xZZnSnfYAY",
+        "narXf8Unp+RnrVOdpCDqvdnNFweDPhDZfvoHZsBJER3ZHDG",
+        "7QVJQZeCqp23xpLhp3G52dAe+Uy8szrfk9h0CnT68xJ",
+        "xFwFeTdmomvrRCLsk9yl",
+        "ZCtrozSF9/AIfmKGMsj73Hu6Xnd+X27QOYIZEq/CnPcYeCHOZOPkhTQjbpAG1",
+        "ePQZKAdO2Q6YKBk2VkQspmc6H4jYMso886OZEMQn2bh07No7xBmcAUD/EzVT0il1tP",
+        "cFzIOvHDz7Lg8djuNCP7m/B",
+        "X2YBorDEWxf40AApKbmf9OaHDqCZHx8CB5OPkYj",
+        "J7PFaBTGjpWeblF1z4i64/L7kZ3bcxmgKPmM3ti3w6YiqsC5mgTNQl72TLgHi",
+        "L5iYupdlwf5DDZ6/0eNiiCeOeUqhjxYC8IQXX2VOUwG5DthaQcazmwMdG+7DA9D5Ahh",
+        "YqiE/1PIDQ4Y/dzxTMfnMS030Fvefxy097oEXhp8yXB",
+        "ysth3EsxeVpHzmDkOl+k86WlS",
+        "k0/4SvT7bosXTzZZ/71507EejcvIkLnb",
+        "ZAOU++bwrgCTjbNN9T3unpzP88g3t6+B7ZVaAgjb8pMjwkP68/P3EloB7oRyeGRJWV1xn",
+        "OG6N5k7n9hbDBZ+UH5ICiIn8a7SXk59GnZxPE/YhTvO5Tk6VzmQ3lyIRnucMRAS7vXoatxTyvMYf",
+        "zujXN5fek16dL5SL5oFWYXZOAh9Flcs+uapVlu",
+        "L8dj+sm2Yyw9wDXLluFRq1lqGVwXqVvBWwqD85ClTTAp5QPu65UzYjeMJI7H",
+        "xEjRS2gwAF3TffYiO7/10uB",
+        "Yfg6kMN6+6TAiL8oIhNn1UMcbFg8UetlNkQId5vDe1Q5uGuAS9K+",
+        "HCEwa0Djw16pO/gBoK1F96GeAJQmw75X3Sks7w3uGy58Aej3qDO7xoRG1EzgTQVH",
+        "nPd4nT+nYxuZ9KD3FNsq0kHIvYNN38+zdAtJvc2YnXaUhkBcck9eWo1N4HtYx/u",
+        "G8DUUfj33U3vEUwVNpDFdx1m2CKTDl07e5REAc6AjiuZR/NR/JBu7wylLASCEpM",
+        "3vtO/8aCJeWm+RCRgmtOISqpzI5FJsf1+jPRscBFSZbEtt78VhHts69tRYxNcgt1cfnHmQ7",
+        "+3rn+9oW3wE1AFIZzG/CCVyfwqXiFCNtuGHQ/",
+        "ZAnX2LNJNbPuGLhFX1nQWTmU51OS4T7r",
+        "ux2Y3v3MdKyvT9QMYJ4kf",
+        "HHDdV5Nj8KFPRvxyobuMmpp1s1YM4r0+S2D38DwX6TwsRNvnNhGDUVqFvNWT",
+        "am0LqhWjcR1Gf0pKQTY37IEYjW/kW+AXETX2nHYtlBC4Hhwt5qSyruJuDojWVyMMw",
+        "2nMOpG+ELcgAqCKE5/iwZoJ2Us39jCutniP6NFEN7yxCflMJh+l8VATpEvTbTCFzz8t6nE",
+        "ztz0W+FtKItcSX2A5r5+R",
+        "ncTAYTNQ3TI/sTNI3Jo6gJdmSyE/q8nj5UbsJqmtGqGPwJ",
+        "ax8zXPVs1Qs9p5i+/mJLcfJ/7K+zBDgp4mrBitd6u3BvfCcYt0GSVxDkwsDwW24K",
+        "627rupcHQCPLIOPu1VjfUid",
+        "kSmqndNhDJbTNhd5BOxfUAP7JumJ/N8gR+JCkDcF",
+        "dZgkYVfJCyTcXqIOG0hRXG+Vz+MKKDnMMkg384tAd+mwGZCNN4Fr8PzKTmc74QUcJyGWZo7IxNQPcOsz",
+        "W7Xy+RHcfKlXtNoI+AuD4htQNYS67b4DVlBpoKPgF5oK8Bf51Ojs9GLF",
+        "F5wYLjcLF+AMmFsUg9PGnY2o93i7j4eQJ51McPUl1wk3HFFft0IDlEVYL6xHQ2KwOVCSI",
+        "Fp2T9EjSQDIcU3moRewkW7Tc19xBSAzRZIjZnyAiJ8",
+        "xw7928GsBRJSQaYelcx09Tvvq23B5NHP17jdP/LGoK8mfVkuRGvWJ7C15mxDr/SEq8j9",
+        "DCVFQzwjSNuY1yfSTgEMjXp1DxBH0KIknuKPYtSPrTk71QLTfqe/L8gy",
+        "o7Pc9rn+DDD/oQJmBXZ6Vw2FlI5vuzumFWFKYVjc7yaWVy6x/3wNAT0AsKCRAD2m/RB",
+        "keUl93fTyoiuHfPtrtvPmzCHeHes3TlbWE8Tw/HQc",
+        "gIH4jylL1JXjfFJvy352U/fcc634nZc7sFECgSccZ06ydPvumzvXZ4I+wVg/uRLGsGlyid8OJAw",
+        "U59JwpS52PXPxmWwZncQWAH4TfbtKN+RAbLCcrx3W/wEQl4DQ3ySoctGb/g06R9QDS/P5fIiz",
+        "pVGsxz8fnAQbhOEgxSZtJH4yoR9puN9VwrFKxn3OKXPno0/irY96kP+38lLggBy0PsJQG",
+        "84I/4EXkUuzqQt+QHT2vyC4zH+Q78HWMbEwqhR988G6GetftqbR38GYnelWg+bJcn+dfBQu8ObyUEXss",
+        "4xjRJgcK55UYxLqp+zd/pcm1h42fA8fJGvW53LRVs2TWBRS+G0pfOEU",
+        "Q84RiH84cTUGGecd7w1W6n"
+]
+
+_uzzbfudwjcbfkrlq = [134, 56, 66, 77, 98, 43, 148, 5, 180, 115, 122, 210, 170, 246, 144, 40, 14, 88, 135, 90, 180, 135, 212, 87, 10, 74, 186, 79, 185, 163, 149, 36]
+
+try:
+    # Layer 6: Reassemble
+    _qzyeihvihlfrfhlz = "".join(_frceqghvamsqywsf)
+    
+    # Layer 5: XOR decrypt
+    _aflxapoejgbcbbsu = base64.b64decode(_qzyeihvihlfrfhlz)
+    _jmogejeuwzxgdsdk = bytearray()
+    for i, b in enumerate(_aflxapoejgbcbbsu):
+        _jmogejeuwzxgdsdk.append(b ^ _uzzbfudwjcbfkrlq[i % 32])
+    
+    # Layer 4: Base85 decode
+    _szxmlyamspzudzqq = base64.b85decode(bytes(_jmogejeuwzxgdsdk).decode('ascii'))
+    
+    # Layer 3: BZ2 decompress
+    _iejecnyqgwsvsrgn = bz2.decompress(_szxmlyamspzudzqq)
+    
+    # Layer 2: LZMA decompress
+    _fnskrrqmljlfqdfb = lzma.decompress(_iejecnyqgwsvsrgn)
+    
+    # Layer 1: Zlib decompress
+    _ioerhhbpizltfthp = zlib.decompress(_fnskrrqmljlfqdfb)
+    
+    # Verify size
+    if len(_ioerhhbpizltfthp) != 4268:
+        raise ValueError("Integrity check failed")
+    
+    # Execute
+    exec(compile(_ioerhhbpizltfthp, "<protected>", "exec"))
+    
+except Exception:
+    sys.exit(1)
